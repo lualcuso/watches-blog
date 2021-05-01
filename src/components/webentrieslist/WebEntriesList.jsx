f477528ce56333828a039e17290398a390f04a08
Luis Cubillo <luis.cubillo.soto@gmail.com>
2021-05-01 15:25:12 -0600

    Adds pagination

--- src/components/webentrieslist/WebEntriesList.jsx
+++ src/components/webentrieslist/WebEntriesList.jsx
@@ -4,27 +4,46 @@ import { ENTRIES_URL } from '../../constants';
 
 import Entry from '../entry/Entry';
 
+import './webentrieslist.scss'
+
 class WebEntriesList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { entries: [] };
+        this.state = { 
+            entries: [],
+            currentPage: 1  
+        };
     }
 
-    async componentDidMount() {
-        const response = await api.get(ENTRIES_URL);
+    componentDidMount() {
+        this.getEntries();
+    }
+
+    async getEntries () {
+        const { currentPage, entries } = this.state;
+        const response = await api.get(ENTRIES_URL + currentPage);
         this.setState({
-            entries: response.data.articles
+            entries: currentPage === 1? response.data.articles : entries.concat(response.data.articles)
         });
     }
 
     render() {
         const { entries } = this.state;
         return (
-            <div>
+            <div className='web-entries-list'>
                 {entries.map((entry, index) => {
                     return <Entry key={index} {...entry} />
                 })}
+                {entries.length && <div className='load-more'>
+                    <button onClick={() => {
+                        this.setState({
+                            currentPage: this.state.currentPage + 1
+                        }, () => {
+                            this.getEntries()
+                        })
+                    }}>Load More Articules</button>
+                </div>}
             </div>
         );
     }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const ENTRIES_URL = 'https://newsapi.org/v2/everything?q=watches&sortBy=publishedAt&apiKey=acde49a5d5d247ecb7d7a866c56844fd'
+export const ENTRIES_URL = 'https://newsapi.org/v2/everything?q=watches&sortBy=relevancy&apiKey=acde49a5d5d247ecb7d7a866c56844fd&pageSize=10&page='
